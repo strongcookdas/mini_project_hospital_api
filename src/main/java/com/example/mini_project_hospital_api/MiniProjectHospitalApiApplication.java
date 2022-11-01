ba8e7cf69f167fc85771c323a651904f60db7473
strongcookdas <goodjav141592@gmail.com>
2022-11-01 09:19:27 +0900

    start mini_project_hospital_api

---
+++ src/main/java/com/example/mini_project_hospital_api/MiniProjectHospitalApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mini_project_hospital_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectHospitalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProjectHospitalApiApplication.class, args);
+    }
+
+}
