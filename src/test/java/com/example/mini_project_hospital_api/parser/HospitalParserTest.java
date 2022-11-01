e3f728bc974bb895ae505649367712fb188391be
strongcookdas <goodjav141592@gmail.com>
2022-11-01 13:42:41 +0900

    exception : nullPoint, Failed to load ApplicationContext 예외 해결 1. nullPoint 에러는 @SpringBootTest를 추가 하지 않아서 생긴 에러 2. Failed to load ApplicationCentext는 DataSource를 설정하지 않아서 생긴 오류

--- src/test/java/com/example/mini_project_hospital_api/parser/HospitalParserTest.java
+++ src/test/java/com/example/mini_project_hospital_api/parser/HospitalParserTest.java
@@ -4,6 +4,7 @@ import com.example.mini_project_hospital_api.domain.Hospital;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -11,27 +12,30 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class HospitalParserTest {
     String line1 = "\"1\",\"의원\",\"01_01_02_P\",\"3620000\",\"PHMA119993620020041100004\",\"19990612\",\"\",\"01\",\"영업/정상\",\"13\",\"영업중\",\"\",\"\",\"\",\"\",\"062-515-2875\",\"\",\"500881\",\"광주광역시 북구 풍향동 565번지 4호 3층\",\"광주광역시 북구 동문대로 24, 3층 (풍향동)\",\"61205\",\"효치과의원\",\"20211115113642\",\"U\",\"2021-11-17 02:40:00.0\",\"치과의원\",\"192630.735112\",\"185314.617632\",\"치과의원\",\"1\",\"0\",\"0\",\"52.29\",\"401\",\"치과\",\"\",\"\",\"\",\"0\",\"0\",\"\",\"\",\"0\",\"\",";
 
     @Autowired
     ReadLineContext<Hospital> hospitalReadLineContext;
-
     @Test
-    @DisplayName("파일 데이터를 잘 파싱하는지")
-    void oneHundreadThousandRows() throws IOException{
+    @DisplayName("10만건 이상 데이터가 파싱 되는지")
+    void oneHundreadThousandRows() throws IOException {
+        // 서버환경에서 build할 때 문제가 생길 수 있습니다.
+        // 어디에서든지 실행할 수 있게 짜는 것이 목표.
         String filename = "hospital.csv";
         List<Hospital> hospitalList = hospitalReadLineContext.readByLine(filename);
         assertTrue(hospitalList.size() > 1000);
         assertTrue(hospitalList.size() > 10000);
-        for (int i = 0; i < 10; i++) {
-            System.out.println(hospitalList.get(i).getHospitalName());
-        }
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println(hospitalList.get(i).getHospitalName());
+//        }
         System.out.printf("파싱된 데이터 개수:", hospitalList.size());
     }
 
+
     @Test
-    @DisplayName("1줄을 잘 파싱하는지")
+    @DisplayName("csv 1줄을 잘만드는지 test")
     void convertToHospital() {
         HospitalParser hp = new HospitalParser();
         Hospital hospital = hp.parse(line1);
@@ -52,4 +56,5 @@ class HospitalParserTest {
         assertEquals(0, hospital.getTotalNumberOfBeds());
         assertEquals(52.29f, hospital.getTotalAreaSize());
     }
+
 }
