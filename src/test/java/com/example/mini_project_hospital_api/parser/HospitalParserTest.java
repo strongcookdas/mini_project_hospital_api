8665bb8b833085278c0c3dc485f6a2a9d47143d4
strongcookdas <goodjav141592@gmail.com>
2022-11-01 11:22:59 +0900

    exception : nullPoint예외 발생 아직 해결 완료하지 못함

---
+++ src/test/java/com/example/mini_project_hospital_api/parser/HospitalParserTest.java
@@ -0,0 +1,55 @@
+package com.example.mini_project_hospital_api.parser;
+
+import com.example.mini_project_hospital_api.domain.Hospital;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HospitalParserTest {
+    String line1 = "\"1\",\"의원\",\"01_01_02_P\",\"3620000\",\"PHMA119993620020041100004\",\"19990612\",\"\",\"01\",\"영업/정상\",\"13\",\"영업중\",\"\",\"\",\"\",\"\",\"062-515-2875\",\"\",\"500881\",\"광주광역시 북구 풍향동 565번지 4호 3층\",\"광주광역시 북구 동문대로 24, 3층 (풍향동)\",\"61205\",\"효치과의원\",\"20211115113642\",\"U\",\"2021-11-17 02:40:00.0\",\"치과의원\",\"192630.735112\",\"185314.617632\",\"치과의원\",\"1\",\"0\",\"0\",\"52.29\",\"401\",\"치과\",\"\",\"\",\"\",\"0\",\"0\",\"\",\"\",\"0\",\"\",";
+
+    @Autowired
+    ReadLineContext<Hospital> hospitalReadLineContext;
+
+    @Test
+    @DisplayName("파일 데이터를 잘 파싱하는지")
+    void oneHundreadThousandRows() throws IOException{
+        String filename = "hospital.csv";
+        List<Hospital> hospitalList = hospitalReadLineContext.readByLine(filename);
+        assertTrue(hospitalList.size() > 1000);
+        assertTrue(hospitalList.size() > 10000);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(hospitalList.get(i).getHospitalName());
+        }
+        System.out.printf("파싱된 데이터 개수:", hospitalList.size());
+    }
+
+    @Test
+    @DisplayName("1줄을 잘 파싱하는지")
+    void convertToHospital() {
+        HospitalParser hp = new HospitalParser();
+        Hospital hospital = hp.parse(line1);
+        assertEquals(1, hospital.getId());
+        assertEquals("의원", hospital.getOpenServiceName());
+        assertEquals(3620000,hospital.getOpenLocalGovernmentCode());
+        assertEquals("PHMA119993620020041100004",hospital.getManagementNumber());
+        assertEquals(LocalDateTime.of(1999, 6, 12, 0, 0, 0), hospital.getLicenseDate()); //19990612
+        assertEquals(1, hospital.getBusinessStatus());
+        assertEquals(13, hospital.getBusinessStatusCode());
+        assertEquals("062-515-2875", hospital.getPhone());
+        assertEquals("광주광역시 북구 풍향동 565번지 4호 3층", hospital.getFullAddress());
+        assertEquals("광주광역시 북구 동문대로 24, 3층 (풍향동)", hospital.getRoadNameAddress());
+        assertEquals("효치과의원", hospital.getHospitalName());
+        assertEquals("치과의원", hospital.getBusinessTypeName());
+        assertEquals(1, hospital.getHealthcareProviderCount());
+        assertEquals(0, hospital.getPatientRoomCount());
+        assertEquals(0, hospital.getTotalNumberOfBeds());
+        assertEquals(52.29f, hospital.getTotalAreaSize());
+    }
+}
