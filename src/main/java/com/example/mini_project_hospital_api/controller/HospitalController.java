bafb563d4f6498f1aecf4c6e68275e70c9b58ab3
strongcookdas <goodjav141592@gmail.com>
2022-11-02 13:14:37 +0900

    chor : HospitalController 수정

--- src/main/java/com/example/mini_project_hospital_api/controller/HospitalController.java
+++ src/main/java/com/example/mini_project_hospital_api/controller/HospitalController.java
@@ -3,10 +3,14 @@ package com.example.mini_project_hospital_api.controller;
 import com.example.mini_project_hospital_api.dao.HospitalDao;
 import com.example.mini_project_hospital_api.domain.Hospital;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/hospital")
@@ -20,8 +24,14 @@ public class HospitalController {
     public String hello(){return "Wellcome to Hospital API";}
 
     @GetMapping("/{id}")
-    public String getHospital(@PathVariable("id") String id){
+    public ResponseEntity<Hospital> getHospital(@PathVariable("id") String id){
         Hospital hospital = hospitalDao.findeById(Integer.parseInt(id));
-        return hospital.toString();
+        Optional<Hospital> opt = Optional.of(hospital);
+
+        if(!opt.isEmpty()){
+            return ResponseEntity.ok().body(hospital);
+        }else{
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Hospital());
+        }
     }
 }
